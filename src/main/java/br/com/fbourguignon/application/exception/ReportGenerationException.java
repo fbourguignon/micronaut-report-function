b37e416ad922420c101c53569eddcca9b0ef2e2c
fbourguignon <franklin_bourguignon@hotmail.com>
2023-07-16 21:43:53 -0300

    feat(adding custom exception)

---
+++ src/main/java/br/com/fbourguignon/application/exception/ReportGenerationException.java
@@ -0,0 +1,7 @@
+package br.com.fbourguignon.application.exception;
+
+public class ReportGenerationException extends RuntimeException {
+    public ReportGenerationException(String message) {
+        super(message);
+    }
+}
